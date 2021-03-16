087a4e616570e9032d6cb3cd33dac76514f2a194
Andy Giesen <argiesen@gmail.com>
2021-03-15 19:01:10 -0700

    Update Install-Sql.ps1

--- SQL/Install-Sql.ps1
+++ SQL/Install-Sql.ps1
@@ -1,7 +1,71 @@
+[cmdletbinding()]
+param (
+	[ValidateNotNullOrEmpty()]
+	[string]$SQLInstance = "MSSQLSERVER",
+	[ValidateNotNullOrEmpty()]
+	[string]$SQLMediaDir,
+	[ValidateNotNullOrEmpty()]
+	[string]$SQLPath = "$env:ProgramFiles\Microsoft SQL Server",
+	[string]$LogPath
+)
+
+function Write-Log {
+	param(
+		[string]$Message,
+		[ValidateSet("File", "Screen", "FileAndScreen")]
+		[string]$OutTo = "FileAndScreen",
+		[string]$LogPath,
+		[ValidateSet("Info", "Warn", "Error", "Verb", "Debug")]
+		[string]$Level = "Info",
+		[ValidateSet("Black", "DarkMagenta", "DarkRed", "DarkBlue", "DarkGreen", "DarkCyan", "DarkYellow", "Red", "Blue", "Green", "Cyan", "Magenta", "Yellow", "DarkGray", "Gray", "White")]
+		[String]$ForegroundColor = "White",
+		[ValidateRange(1,30)]
+		[int]$Indent = 0,
+		[switch]$Clobber,
+		[switch]$NoNewLine
+	)
+	
+	if ($LogPath -eq $null -or $LogPath -eq ""){
+		if ($GlobalLogPath){
+			$LogPath = $GlobalLogPath
+		}else{
+			$LogPath = "$($env:ComputerName)-$(Get-Date -f yyyyMMdd).log"
+		}
+	}
+	
+	$msg = "{0} : {1} : {2}{3}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $Level.ToUpper(), ("  " * $Indent), $Message
+	if ($OutTo -match "File"){
+		if (($Level -ne "Verb") -or ($VerbosePreference -eq "Continue")){
+			if ($Clobber){
+				$msg | Out-File $LogPath -Force
+			}else{
+				$msg | Out-File $LogPath -Append
+			}
+		}
+	}
+	
+	$msg = "{0}{1}" -f ("  " * $Indent), $Message
+	if ($OutTo -match "Screen"){
+		switch ($Level){
+			"Info" {
+				if ($NoNewLine){
+					Write-Host $msg -ForegroundColor $ForegroundColor -NoNewLine
+				}else{
+					Write-Host $msg -ForegroundColor $ForegroundColor
+				}
+			}
+			"Warn" {Write-Warning $msg}
+			"Error" {$host.ui.WriteErrorLine($msg)}
+			"Verb" {Write-Verbose $msg}
+			"Debug" {Write-Debug $msg}
+		}
+	}
+}
+
 function Install-SQLInstance {
 	param (
 		[ValidateNotNullOrEmpty()]
-		[string]$Instance,
+		[string]$Instance = "MSSQLSERVER",
 		[ValidateNotNullOrEmpty()]
 		[string]$SQLPath = "$env:ProgramFiles\Microsoft SQL Server",
 		[ValidateNotNullOrEmpty()]
@@ -12,7 +76,7 @@ function Install-SQLInstance {
 	)
 	
 	if (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL" -Name "$Instance" -ErrorAction SilentlyContinue){
-		Write-Log "SQL Express 2014 ($Instance) already installed" -Indent $Indent -OutTo $LogOutTo
+		Write-Log "SQL Instance ($Instance) already installed" -Indent $Indent -OutTo $LogOutTo
 		return
 	}
 	
@@ -57,63 +121,16 @@ function Install-SQLInstance {
 	}
 }
 
-function Write-Log {
-	param(
-		[string]$Message,
-		[ValidateSet("File", "Screen", "FileAndScreen")]
-		[string]$OutTo = "FileAndScreen",
-		[ValidateSet("Info", "Warn", "Error", "Verb", "Debug")]
-		[string]$Level = "Info",
-		[ValidateSet("Black", "DarkMagenta", "DarkRed", "DarkBlue", "DarkGreen", "DarkCyan", "DarkYellow", "Red", "Blue", "Green", "Cyan", "Magenta", "Yellow", "DarkGray", "Gray", "White")]
-		[String]$ForegroundColor = "White",
-		[ValidateRange(1,30)]
-		[int]$Indent = 0,
-		[switch]$Clobber,
-		[switch]$NoNewLine
-	)
-	
-	if (!($LogPath)){
-		$LogPath = "$($env:ComputerName)-$(Get-Date -f yyyyMMdd).log"
-	}
-	
-	$msg = "{0} : {1} : {2}{3}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $Level.ToUpper(), ("  " * $Indent), $Message
-	if ($OutTo -match "File"){
-		if (($Level -ne "Verb") -or ($VerbosePreference -eq "Continue")){
-			if ($Clobber){
-				$msg | Out-File $LogPath -Force
-			}else{
-				$msg | Out-File $LogPath -Append
-			}
-		}
-	}
-	
-	$msg = "{0}{1}" -f ("  " * $Indent), $Message
-	if ($OutTo -match "Screen"){
-		switch ($Level){
-			"Info" {
-				if ($NoNewLine){
-					Write-Host $msg -ForegroundColor $ForegroundColor -NoNewLine
-				}else{
-					Write-Host $msg -ForegroundColor $ForegroundColor
-				}
-			}
-			"Warn" {Write-Warning $msg}
-			"Error" {$host.ui.WriteErrorLine($msg)}
-			"Verb" {Write-Verbose $msg}
-			"Debug" {Write-Debug $msg}
-		}
-	}
-}
+#$InstallDrive = "C:"
+#$SoftwareDir = "C:\software"
+#$Indent = 1
+#$UserTempDir = [environment]::GetEnvironmentVariable("temp","user")
+#$LogPath = "C:\software\InstallSQL.txt"
 
-$InstallDrive = "C:"
-$SoftwareDir = "C:\software"
-$Indent = 1
-$UserTempDir = [environment]::GetEnvironmentVariable("temp","user")
-$LogPath = "C:\software\InstallSQL.txt"
-
-Write-Log "Extracting SQL Express 2014"
-$Process = Start-Process -FilePath "$SoftwareDir\SQLEXPR_x64_ENU.exe" -ArgumentList /q, /x:"$UserTempDir\SQLEXPR_x64_ENU" -Wait -Passthru -Verb RunAs
-if ($process.ExitCode -ne 0){throw "$SoftwareDir\SQLEXPR_x64_ENU.exe /x: returned error code: $($process.ExitCode)"}
+#Write-Log "Extracting SQL Express 2014"
+#$Process = Start-Process -FilePath "$SoftwareDir\SQLEXPR_x64_ENU.exe" -ArgumentList /q, /x:"$UserTempDir\SQLEXPR_x64_ENU" -Wait -Passthru -Verb RunAs
+#if ($process.ExitCode -ne 0){throw "$SoftwareDir\SQLEXPR_x64_ENU.exe /x: returned error code: $($process.ExitCode)"}
 
 #Write-Log "Installing RTC" -OutTo $LogOutTo
-Install-CsSQLInstance -Instance RTC -SQLMediaDir $UserTempDir -SQLPath "$InstallDrive\Program Files\Microsoft SQL Server" -OpenPorts | Out-Null
+#Install-CsSQLInstance -Instance RTC -SQLMediaDir $UserTempDir -SQLPath "$InstallDrive\Program Files\Microsoft SQL Server" -OpenPorts | Out-Null
+Install-CsSQLInstance -Instance $SQLInstance -SQLMediaDir $SQLMediaDir -SQLPath $SQLPath -OpenPorts | Out-Null
